d2ea6cac052fdc15765c0c64f38aaaec170a6f1b
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-20 22:31:35 +0200

    added auth for admin in endpoints

--- Server/src/resources/orders/order-router.ts
+++ Server/src/resources/orders/order-router.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { auth } from "../../middlewares/auth";
+import { authAdmin } from "../../middlewares/authAdmin";
 import { getAllOrders, getOneOrder, getUserOrders, markAsShipped, registerOrder } from "./order.controller";
 
 export const orderRouter = express
   .Router()
   .post("/api/orders/add", auth, registerOrder)
   .get("/api/orders", auth, getAllOrders)
-  .put("/api/orders/:id", auth, markAsShipped)
+  .put("/api/orders/:id", auth, authAdmin, markAsShipped)
   .get("/api/orders/:id", auth, getOneOrder)
   .get("/api/orders/user/:id", auth, getUserOrders)
