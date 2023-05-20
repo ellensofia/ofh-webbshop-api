e3c87f5745d407843dc83ac0699e6ec380f9df48
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-20 21:54:09 +0200

    export models and types

---
+++ Server/src/index.ts
@@ -0,0 +1,6 @@
+export * from './app';
+export { User, UserModel } from './resources/users/user-model';
+export { Product, ProductModel } from './resources/products/product-model';
+export { Order, OrderModel } from './resources/orders/order-model';
+export { Image, ImageModel } from './resources/images/image-model';
+export { Category, CategoryModel } from './resources/categories/category-model';
