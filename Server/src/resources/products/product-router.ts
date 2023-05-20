4dd2acf931dbe008e6cee6e1aa51e8bacf743b42
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-20 21:54:29 +0200

    added auth middleware to endpoints

--- Server/src/resources/products/product-router.ts
+++ Server/src/resources/products/product-router.ts
@@ -1,11 +1,12 @@
 import express from "express";
+import { auth } from "../../middlewares/auth";
 import { addProduct, deleteProduct, getAllProducts, getAllProductsOfCategory, getProduct, updateProduct } from "./product-controller";
 
 export const userRouter = express
   .Router()
-  .post("/api/products/add", addProduct)
+  .post("/api/products/add", auth, addProduct)
   .get("/api/products", getAllProducts)
   .get("/api/products/category/:id", getAllProductsOfCategory)
   .get("/api/products/:id", getProduct)
-  .put("/api/products/:id", updateProduct)
-  .delete("/api/products/:id", deleteProduct)
+  .put("/api/products/:id", auth, updateProduct)
+  .delete("/api/products/:id", auth, deleteProduct)
