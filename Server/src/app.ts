4784807e3bbb15008525eba0ae02d93f71ce1158
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-20 22:25:47 +0200

    added routes middlewares

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -1,5 +1,8 @@
 import cookieSession from "cookie-session";
 import express, { NextFunction, Request, Response } from 'express';
+import { categoryRouter } from "./resources/categories/category-router";
+import { imageRouter } from "./resources/images/image-router";
+import { orderRouter } from "./resources/orders/order-router";
 import { productRouter } from "./resources/products/product-router";
 import { userRouter } from "./resources/users/user-router";
 
@@ -16,6 +19,9 @@ app.use(cookieSession({
 }))
 app.use(userRouter)
 app.use(productRouter)
+app.use(orderRouter)
+app.use(categoryRouter)
+app.use(imageRouter)
 
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
