488d55c79590afea69c69772d7388b61ebc4fdb9
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-22 14:08:29 +0200

    changed "name" to "title" and changed ImageUrl to imageUrl

--- Server/src/resources/products/product-model.ts
+++ Server/src/resources/products/product-model.ts
@@ -1,14 +1,14 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
 export const productSchema = new Schema({
-  name: { type: String, required: true, minlength: 3 },
+  title: { type: String, required: true, minlength: 3 },
   imageId: { type: String, required: true },
-  ImageUrl: { type: String, required: true },
+  imageUrl: { type: String, required: true },
   categories: { type: Array, required: true },
   inStockAmount: { type: Number, required: true },
   price: { type: Number, required: true },
   brand: { type: String, required: true },
-  isArchived: { type: Boolean, required: true }
+  isArchived: { type: Boolean, required: true },
 });
 
 export type Product = InferSchemaType<typeof productSchema>;
