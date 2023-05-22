b42973ddf86920931c1e26b385b6d1ce72b56f39
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-22 14:59:06 +0200

    added getOneProduct function

--- Server/src/resources/products/product-controller.ts
+++ Server/src/resources/products/product-controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { ProductModel } from "./product-model";
+import mongoose from "mongoose";
 
 export async function registerProduct(req: Request, res: Response) {
   return console.log("Add Product");
@@ -14,9 +15,22 @@ export async function getAllProducts(req: Request, res: Response) {
 export async function getAllProductsFromCategory(req: Request, res: Response) {
   return console.log("Get All Products From Category");
 }
+
 export async function getOneProduct(req: Request, res: Response) {
+  const productId = req.params._id;
+  // Check if the provided postId is a valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    return res.status(400).json({ error: `Invalid post ID.` });
+  }
+  const product = await ProductModel.findById(productId);
+  if (product) {
+    res.status(200).json(product);
+  } else {
+    res.status(404).json(`/${productId} not found.`);
+  }
   return console.log("Get Product");
 }
+
 export async function editProduct(req: Request, res: Response) {
   return console.log("Update Product");
 }
