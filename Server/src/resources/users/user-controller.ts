cf298b23249cfe6a827fcd0444d9c18cb30415b5
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-22 13:16:50 +0200

    login endpoint complete

--- Server/src/resources/users/user-controller.ts
+++ Server/src/resources/users/user-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from "express";
+import { UserModel } from "./user-model";
+import argon2 from "argon2";
 
 export async function registerUser(
   req: Request,
@@ -30,7 +32,35 @@ export async function loginUser(
   req: Request,
   res: Response
 ) {
-    return console.log('Login User')
+  const { email, password } = req.body;
+  const user = await UserModel.findOne({
+    email,
+  });
+
+  if (!user) {
+    res.status(401).json("Incorrect email or password");
+    return;
+  }
+  const isAuth = await argon2.verify(
+    user.password,
+    password
+  );
+  if (!isAuth) {
+    res.status(401).json("Incorrect email or password");
+    return;
+  }
+
+  req.session!.username = user.username;
+  req.session!.email = user.email;
+  req.session!._id = user._id;
+  req.session!.isAdmin = user.isAdmin;
+
+  res.status(200).json({
+    _id: user!._id,
+    username: user!.username,
+    email: user!.email,
+    isAdmin: user!.isAdmin,
+  });
 }
 export async function logoutUser(
   req: Request,
