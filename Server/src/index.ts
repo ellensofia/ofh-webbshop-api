80a30f93e7d310830499554a9e022563fa348764
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-05-22 15:06:30 +0200

    Fix: References to Image types removed from server/index.ts

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,6 +1,5 @@
-export * from './app';
-export { User, UserModel } from './resources/users/user-model';
-export { Product, ProductModel } from './resources/products/product-model';
-export { Order, OrderModel } from './resources/orders/order-model';
-export { Image, ImageModel } from './resources/images/image-model';
-export { Category, CategoryModel } from './resources/categories/category-model';
+export * from "./app";
+export { Category, CategoryModel } from "./resources/categories/category-model";
+export { Order, OrderModel } from "./resources/orders/order-model";
+export { Product, ProductModel } from "./resources/products/product-model";
+export { User, UserModel } from "./resources/users/user-model";
