11ac9c3f1bdbe3aabc9699da091ea497ef5401e0
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-22 11:34:25 +0200

    add functionality in getAllProducts

--- Server/src/resources/products/product-controller.ts
+++ Server/src/resources/products/product-controller.ts
@@ -1,40 +1,25 @@
 import { Request, Response } from "express";
+import { ProductModel } from "./product-model";
 
-export async function registerProduct(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Add Product')
-  }
-  
-  export async function getAllProducts(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Get All Products')
-  }
-  
-  export async function getAllProductsFromCategory(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Get All Products From Category')
-  }
-  export async function getOneProduct(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Get Product')
-  }
-  export async function editProduct(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Update Product')
-  }
-  export async function deleteProduct(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Delete Product')
-  }
+export async function registerProduct(req: Request, res: Response) {
+  return console.log("Add Product");
+}
+
+export async function getAllProducts(req: Request, res: Response) {
+  const products = await ProductModel.find({});
+  res.status(200).json(products);
+  return console.log("Get All Products");
+}
+
+export async function getAllProductsFromCategory(req: Request, res: Response) {
+  return console.log("Get All Products From Category");
+}
+export async function getOneProduct(req: Request, res: Response) {
+  return console.log("Get Product");
+}
+export async function editProduct(req: Request, res: Response) {
+  return console.log("Update Product");
+}
+export async function deleteProduct(req: Request, res: Response) {
+  return console.log("Delete Product");
+}
