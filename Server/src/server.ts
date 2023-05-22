05dfb41dd49e9885209ff26947747df7a3e60f78
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-05-22 16:07:10 +0200

    temp database and removed middlewares

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 async function main() {
-    await mongoose.connect('mongodb+srv://admin:zVUpSNFeT46dGCuI@ofh-webbshop-api.glo7kd3.mongodb.net/')
+    // await mongoose.connect('mongodb+srv://admin:zVUpSNFeT46dGCuI@ofh-webbshop-api.glo7kd3.mongodb.net/')
+    await mongoose.connect('mongodb://127.0.0.1:27017/twitter')
     console.log('Connected to database');
 
     app.listen(3000, () => console.log('Running on http://localhost:3000'))
