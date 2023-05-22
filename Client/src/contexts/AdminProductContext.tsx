a80208fe233061905751ce1166c4a5b5a6bc7d2a
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-22 14:15:39 +0200

    added product interface + getAllProducts

--- Client/src/contexts/AdminProductContext.tsx
+++ Client/src/contexts/AdminProductContext.tsx
@@ -1,13 +1,27 @@
-import React, { createContext, useContext } from "react";
-import { Product, products as mockedProducts } from "../../data";
-import { useLocalStorageState } from "../hooks/useLocalStorageState";
+import React, { createContext, useContext, useState } from "react";
+// import { Product } from "../../data";
 
 interface Props {
   children: React.ReactNode;
 }
 
+export interface Product {
+  _id: string;
+  imageUrl: string;
+  title: string;
+  brand?: string;
+  description: string;
+  price: number;
+  timestamp: string;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
 type ProductContextType = {
   products: Product[];
+  getAllProducts: () => void;
   addProduct: (product: Product) => void;
   removeProduct: (product: Product) => void;
   editProduct: (editedProduct: Product) => void;
@@ -18,8 +32,13 @@ type ProductContextType = {
 // providing default values for the product list and CRUD operations.
 const AdminProductContext = createContext<ProductContextType>({
   products: [],
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  getAllProducts: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   addProduct: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   editProduct: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   removeProduct: () => {},
 });
 
@@ -27,38 +46,41 @@ const AdminProductContext = createContext<ProductContextType>({
 export const useProduct = () => useContext(AdminProductContext);
 
 export const ProductProvider = ({ children }: Props) => {
-  const [products, setProducts] = useLocalStorageState<Product[]>(
-    mockedProducts,
-    "products"
-  );
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getAllProducts = async () => {
+    try {
+      const response = await fetch("/api/products");
+      if (!response.ok) {
+        throw new Error("Failed to fetch products.");
+      }
+      const products = await response.json();
+      setProducts(products);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const addProduct = (product: Product) => {
     setProducts([...products, product]);
   };
 
   const editProduct = (editedProduct: Product) => {
-    setProducts(
-      products.map((product) =>
-        product.id === editedProduct.id ? editedProduct : product
-      )
-    );
+    setProducts(products.map((product) => (product._id === editedProduct._id ? editedProduct : product)));
   };
 
   const removeProduct = (product: Product) => {
-    setProducts(products.filter((p) => p.id !== product.id));
+    setProducts(products.filter((p) => p._id !== product._id));
   };
 
   const productContext: ProductContextType = {
     products,
+    getAllProducts,
     addProduct,
     editProduct,
     removeProduct,
   };
 
   // Renders the child components wrapped inside the AdminProductContext.Provider
-  return (
-    <AdminProductContext.Provider value={productContext}>
-      {children}
-    </AdminProductContext.Provider>
-  );
+  return <AdminProductContext.Provider value={productContext}>{children}</AdminProductContext.Provider>;
 };
