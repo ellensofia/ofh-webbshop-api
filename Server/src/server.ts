32db54561d6d8650a6b3a304a085f07f0ac85e0a
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-05-23 12:01:51 +0200

    change database

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,12 +1,11 @@
-import mongoose from 'mongoose';
-import { app } from './app';
+import mongoose from "mongoose";
+import { app } from "./app";
 
 async function main() {
-    // await mongoose.connect('mongodb+srv://admin:zVUpSNFeT46dGCuI@ofh-webbshop-api.glo7kd3.mongodb.net/')
-    await mongoose.connect('mongodb://127.0.0.1:27017/twitter')
-    console.log('Connected to database');
+  await mongoose.connect("mongodb+srv://admin:zVUpSNFeT46dGCuI@ofh-webbshop-api.glo7kd3.mongodb.net/");
+  console.log("Connected to database");
 
-    app.listen(3000, () => console.log('Running on http://localhost:3000'))
+  app.listen(3000, () => console.log("Running on http://localhost:3000"));
 }
 
 main().catch(console.error);
