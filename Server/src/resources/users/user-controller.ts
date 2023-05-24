7fe8dead74f9321f381837dcefb1c74c28ef109e
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-24 10:57:35 +0200

    endpoint to change user role complete

--- Server/src/resources/users/user-controller.ts
+++ Server/src/resources/users/user-controller.ts
@@ -62,8 +62,35 @@ export async function getAllUsers(req: Request, res: Response) {
 }
 
 export async function changeRole(req: Request, res: Response) {
-  return console.log("Update User");
+  const loggedInUser = req.session;
+  const user = await UserModel.findOne({ _id: loggedInUser?._id });
+
+  if (!user?.isAdmin) {
+    res.status(403).json("Not authorized to change users role!");
+    return;
+  }
+
+  const { username, email, isAdmin } = req.body;
+
+  const foundUser = await UserModel.findByIdAndUpdate(
+    req.params.id,
+    { username, email, isAdmin },
+    { new: true, select: "-password" },
+  );
+
+  if (!foundUser) {
+    res.status(404).json(`User ${req.params.id} was not found.`);
+    return;
+  }
+
+  return res.status(200).json({
+    username: foundUser.username,
+    email: foundUser.email,
+    isAdmin: foundUser.isAdmin,
+    _id: foundUser._id.toString(),
+  });
 }
+
 export async function getOneUser(req: Request, res: Response) {
   return console.log("Get User");
 }
