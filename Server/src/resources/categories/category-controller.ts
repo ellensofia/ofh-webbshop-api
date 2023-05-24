9235f2711262c725f74a10ce1b44503f5c732a77
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-05-24 13:00:44 +0200

    add and fetch all categories, categories are now saved on server and not db

--- Server/src/resources/categories/category-controller.ts
+++ Server/src/resources/categories/category-controller.ts
@@ -1,15 +1,33 @@
 import { Request, Response } from "express";
 
-export async function getAllCategories(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Get All Categories')
-  }
+const categories: string[] = [];
 
-  export async function getSpecificCategories(
-    req: Request,
-    res: Response
-  ) {
-      return console.log('Get Category')
+export async function createCategory(req: Request, res: Response) {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(400).json({ message: "Name is required" });
+    }
+
+    categories.push(name);
+    console.log("categories: ", categories); // Skriv ut kategorierna i konsolen för teständamål
+
+    res.status(201).json({ message: "Category created successfully", categories: categories });
+  } catch (error) {
+    console.error("Error creating category:", error);
+    res.status(500).json({ message: "Failed to create category" });
   }
+}
+
+export async function getAllCategories(req: Request, res: Response) {
+  try {
+    res.json(categories);
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    res.status(500).json({ message: "Failed to fetch categories" });
+  }
+}
+
+export async function getSpecificCategories(req: Request, res: Response) {
+  return console.log("Get Category");
+}
--- Server/src/resources/categories/category-router.ts
+++ Server/src/resources/categories/category-router.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getAllCategories, getSpecificCategories } from "./category-controller";
+import { createCategory, getAllCategories, getSpecificCategories } from "./category-controller";
 
 export const categoryRouter = express
   .Router()
+  .post("/api/categories/add", createCategory)
   .get("/api/categories", getAllCategories)
-  .get("/api/categories/:id", getSpecificCategories)
+  .get("/api/categories/:id", getSpecificCategories);
