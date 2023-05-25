642cfb86dad6b77448b4488259885d9638b4a5c0
ParhamInBinart <parham.berenjian@gmail.com>
2023-05-25 17:04:22 +0200

    displaying order details logic + styling

--- Client/src/components/OrderDetails.tsx
+++ Client/src/components/OrderDetails.tsx
@@ -1,25 +1,25 @@
 import { Box, Container, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { Order } from "../contexts/OrderContext";
-import { useUserContext } from "../contexts/UserContext";
-import { UserOrder } from "./UserOrder";
 
 export function OrderDetails() {
-  const [myOrders, setMyOrders] = useState<Order[]>([]);
-  const { user } = useUserContext();
+  const { id } = useParams<{ id: string }>();
+  const [order, setOrder] = useState<Order>();
 
-  const getUserOrders = async () => {
-    const response = await fetch(`/api/orders/user/${user?._id}`);
-    const data = await response.json();
+  const getAllOrders = async () => {
+    const response = await fetch("/api/orders");
+    const allOrders = await response.json();
 
     if (response.ok) {
-      setMyOrders(data);
+      setOrder(allOrders.find((ord: Order) => ord._id === id));
     }
   };
+  console.log(order);
 
   useEffect(() => {
-    getUserOrders();
-  });
+    getAllOrders();
+  }, []);
 
   return (
     <Container maxWidth="md">
@@ -32,36 +32,58 @@ export function OrderDetails() {
           marginBottom: "1rem",
         }}
       >
-        <h2>My orders</h2>
+        <h2>{"Order id: " + order?._id}</h2>
+        <span>{order?.isShipped ? <h4>Status: Shipped</h4> : <h4>Status: Pending shippment</h4>}</span>
       </Box>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
             <TableRow>
               <TableCell align="left" sx={{ fontSize: "1.5rem" }}>
-                Order ID
+                Title
               </TableCell>
               <TableCell align="center" sx={{ fontSize: "1.5rem" }}>
-                Date
+                Desc
               </TableCell>
               <TableCell align="center" sx={{ fontSize: "1.5rem" }}>
-                Products
+                Quantity
               </TableCell>
               <TableCell align="center" sx={{ fontSize: "1.5rem" }}>
                 Price
               </TableCell>
-              <TableCell align="right" sx={{ fontSize: "1.5rem" }}>
-                Status
-              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {myOrders.map((order) => (
-              <UserOrder key={order._id} order={order} />
-              ))}
+            {order?.orderItems.map((items) => (
+              <TableRow key={items._id}>
+                <TableCell align="left" sx={{ fontSize: "1rem" }}>
+                  <div>{items.title}</div>
+                </TableCell>
+                <TableCell align="center" sx={{ fontSize: "1rem" }}>
+                  <div>{items.description}</div>
+                </TableCell>
+                <TableCell align="center" sx={{ fontSize: "1rem" }}>
+                  <div>{items.quantity}</div>
+                </TableCell>
+                <TableCell align="center" sx={{ fontSize: "1rem" }}>
+                  <div>{items.price} SEK</div>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginTop: "2rem",
+          marginBottom: "1rem",
+        }}
+      >
+        <h3>Total: {order?.price} SEK</h3>
+      </Box>
     </Container>
   );
 }
