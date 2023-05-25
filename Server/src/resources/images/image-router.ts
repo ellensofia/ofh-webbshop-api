04d95be08eb280dc086ad3f96014756406c28d1a
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-25 15:31:39 +0200

    added auth before on delete in image-router (need to add authAdmin aswell later)

--- Server/src/resources/images/image-router.ts
+++ Server/src/resources/images/image-router.ts
@@ -2,6 +2,7 @@ import express from "express";
 // import { auth } from "../../middlewares/auth";
 // import { authAdmin } from "../../middlewares/authAdmin";
 import { deleteImage, getImage, uploadImage } from "./image-controller";
+import { auth } from "../../middlewares/auth";
 
 
 // TODO: Reinstate auth and authAdmin (or just authAdmin?)
@@ -9,4 +10,4 @@ export const imageRouter = express
   .Router()
   .post("/api/images", uploadImage)
   .get("/api/images/:id", getImage)
-  .delete("/api/images/:id", deleteImage)
+  .delete("/api/images/:id", auth, deleteImage)
