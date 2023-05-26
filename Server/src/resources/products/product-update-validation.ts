fdd349ceebedfc158bdfaf14c3366a99bcd35702
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-26 15:02:39 +0200

    made a validation to remove later :))

--- Server/src/resources/products/product-update-validation.ts
+++ Server/src/resources/products/product-update-validation.ts
@@ -3,15 +3,13 @@ import * as Yup from "yup";
 const productUpdateValidationSchema = Yup.object()
   .shape({
     title: Yup.string().required(),
-    imageId: Yup.string(),
-    categories: Yup.string(),
-    description: Yup.string(),
-    inStockAmount: Yup.number(),
-    price: Yup.number(),
-    brand: Yup.string(),
-    isArchived: Yup.boolean(),
-    createdAt: Yup.string().required(),
+    imageId: Yup.string().required(),
+    categories: Yup.array().required(),
+    description: Yup.string().required(),
+    inStockAmount: Yup.number().required(),
+    price: Yup.number().required(),
+    brand: Yup.string().required(),
+    isArchived: Yup.boolean().required(),
   })
-  .noUnknown();
 
 export default productUpdateValidationSchema;
