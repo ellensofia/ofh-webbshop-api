3d9207f269e9b3fc25a2f7dad79a289eed158858
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-05-27 20:10:36 +0200

    remove unnecessary route and function

--- Server/src/resources/categories/category-router.ts
+++ Server/src/resources/categories/category-router.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { createCategory, getAllCategories, getSpecificCategories } from "./category-controller";
+import { createCategory, getAllCategories } from "./category-controller";
 
 export const categoryRouter = express
   .Router()
   .post("/api/categories/add", createCategory)
-  .get("/api/categories", getAllCategories)
-  .get("/api/categories/:id", getSpecificCategories);
+  .get("/api/categories", getAllCategories);
