91ef32b55c86a27c9e744fed2b273bd888d4d4cc
NathanaelBlackbourn <nathanaelblackbourn@gmail.com>
2023-05-30 11:27:24 +0200

    Fix: Removed redundant getOneUser endpoint.

--- Server/src/resources/users/user-router.ts
+++ Server/src/resources/users/user-router.ts
@@ -2,15 +2,7 @@ import express from "express";
 import { auth } from "../../middlewares/auth";
 import { authAdmin } from "../../middlewares/authAdmin";
 import { userSchema, validateBody, validateId } from "../../middlewares/validation";
-import {
-  changeRole,
-  checkUserInfo,
-  getAllUsers,
-  getOneUser,
-  loginUser,
-  logoutUser,
-  registerUser,
-} from "./user-controller";
+import { changeRole, checkUserInfo, getAllUsers, loginUser, logoutUser, registerUser } from "./user-controller";
 
 export const userRouter = express
   .Router()
@@ -19,5 +11,4 @@ export const userRouter = express
   .post("/api/users/login", validateBody(userSchema.omit(["username"])), loginUser)
   .post("/api/users/logout", auth, logoutUser)
   .get("/api/users/checkUserInfo", auth, checkUserInfo)
-  .put("/api/users/:id", validateId, auth, authAdmin, changeRole)
-  .get("/api/users/:id", validateId, authAdmin, getOneUser);
+  .put("/api/users/:id", validateId, auth, authAdmin, changeRole);
