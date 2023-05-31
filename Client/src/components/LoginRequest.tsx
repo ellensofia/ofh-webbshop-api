0bae0a99e286d0d9933bd31cebac4da35b7c97f6
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-05-31 20:08:19 +0200

    fixed responsivity on text

--- Client/src/components/LoginRequest.tsx
+++ Client/src/components/LoginRequest.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography, useMediaQuery } from "@mui/material";
+import { Container, Typography, useMediaQuery } from "@mui/material";
 import { Link } from "react-router-dom";
 import { theme } from "../theme/theme";
 
@@ -12,20 +12,19 @@ export function LoginRequet() {
           width: isSmallScreen ? "sm" : "md",
           marginTop: "5rem",
           display: "flex",
-          alignItems: 'center',
-          gap: '1rem',
+          alignItems: "center",
+          gap: "1rem",
           flexDirection: "column",
-
         }}
       >
-        <Typography marginLeft={"0.5rem"} sx={{ fontSize: "1.1rem" }}>
-          You have to {" "}
+        <Typography marginLeft={"0.5rem"} sx={{ fontSize: "1.1rem", textAlign: "center" }}>
+          Please {" "}
           <Link to={"/login"} style={{ color: "black" }}>
             login
           </Link>{" "}
           to make a purchase
         </Typography>
-        <Typography marginLeft={"0.5rem"} sx={{ fontSize: "1rem" }}>
+        <Typography marginLeft={"0.5rem"} sx={{ fontSize: "1rem", textAlign: "center" }}>
           Don't have an account?{" "}
           <Link to={"/register"} style={{ color: "black" }}>
             Sign up
