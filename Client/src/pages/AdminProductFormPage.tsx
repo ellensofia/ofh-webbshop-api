695dec30df2109e3fe2d17e94a3b774ce3b42e3d
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-06-01 12:35:17 +0200

    move logic to find product from addProductForm to parent component

--- Client/src/pages/AdminProductFormPage.tsx
+++ Client/src/pages/AdminProductFormPage.tsx
@@ -1,15 +1,21 @@
+import { useParams } from "react-router-dom";
 import AddProductForm from "../components/AddProductForm";
 import Footer from "../components/Footer";
 import HeaderMain from "../components/HeaderMain";
+import { useProduct } from "../contexts/AdminProductContext";
 
 function AdminProductFormPage() {
+  const { id } = useParams<{ id: string }>();
+  const { products } = useProduct();
+  const product = products.find((p) => p._id === id);
+
   return (
     <div>
       <header>
         <HeaderMain />
       </header>
       <main>
-        <AddProductForm />
+        <AddProductForm product={product} />
       </main>
       <footer>
         <Footer />
